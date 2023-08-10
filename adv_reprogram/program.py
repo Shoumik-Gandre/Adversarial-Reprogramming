8f505ddda0d23eae9c89ca1b2ec15c7c49922a8a
Shoumik-Gandre <shoumikgandre@gmail.com>
2023-08-10 14:06:56 -0700

    initial commit

---
+++ adv_reprogram/program.py
@@ -0,0 +1,99 @@
+from abc import ABC, abstractmethod
+import math
+from typing import Callable
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+AdapterFn = Callable[[torch.Tensor], torch.Tensor]
+OutputMapperFn = Callable[[torch.Tensor], torch.Tensor]
+
+
+class GenericProgram(nn.Module, ABC):
+
+    # Type Annotations
+    # mean: torch.Tensor
+    # std: torch.Tensor
+    mask: torch.Tensor
+
+    def __init__(self, attack_dims: tuple[int, ...], victim_dims: tuple[int, ...], mask: torch.Tensor, network: nn.Module) -> None:
+        super().__init__()
+        self.register_buffer('mask', mask)
+        self.W = nn.Parameter(torch.randn(victim_dims), requires_grad=True)
+        self.network = network
+        self.attack_dims = attack_dims
+        self.victim_dims = victim_dims
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # h_f(.)
+        X = self.adapter_fn(x).requires_grad_()
+        P = torch.tanh(self.W * self.mask)
+        X_adv = X + P
+        # f(.)
+        Y_adv = F.softmax(self.network(X_adv), dim=1)
+        # h_g(.)
+        return self.output_mapper(Y_adv)
+    
+    def get_program(self) -> torch.Tensor:
+        return torch.tanh(self.W * self.mask)
+    
+    @abstractmethod
+    def adapter_fn(self, x: torch.Tensor) -> torch.Tensor:
+        """This function adapts the dimensions of x into the dimensions that are suitable as the input of the network
+
+        Args:
+            x (torch.Tensor): _description_
+
+        Returns:
+            torch.Tensor: The X in the adversarial programming paper
+        """
+        pass
+
+    @abstractmethod
+    def output_mapper(self, y: torch.Tensor) -> torch.Tensor:
+        """This function adapts the label space from the model to the label space of target tast
+
+        Args:
+            y (torch.Tensor): _description_
+
+        Returns:
+            torch.Tensor: The label y for target task
+        """
+        pass
+        
+
+class MNISTReprogram(GenericProgram):
+
+    def __init__(self, attack_dims: tuple[int, ...], victim_dims: tuple[int, ...], network: nn.Module) -> None:
+        mask = self._get_mask(attack_dims, victim_dims)
+        super().__init__(attack_dims, victim_dims, mask, network)
+
+    def _get_mask(self, in_dimensions: tuple[int, ...], output_dimensions: tuple[int, ...]) -> torch.Tensor:
+        in_channels, in_height, in_width = in_dimensions
+        w_center = math.ceil(in_width/2)
+        h_center = math.ceil(in_height/2)
+        w_start  = w_center - in_width // 2
+        w_end    = w_start + in_width
+        h_start  = h_center - in_height // 2
+        h_end    = h_start + in_height
+
+        M = torch.ones(output_dimensions, dtype=torch.float)
+        M[:, h_start:h_end, w_start:w_end] = 0
+        return M
+
+    def adapter_fn(self, x: torch.Tensor) -> torch.Tensor:
+        X = torch.zeros((x.shape[0], *self.victim_dims), device=x.device)
+
+        start_x = self.victim_dims[1] // 2 - self.attack_dims[1] // 2
+        end_x = start_x + self.attack_dims[1] 
+        start_y = self.victim_dims[2] // 2 - self.attack_dims[2] // 2
+        end_y = start_y + self.attack_dims[2]
+
+        x = x.expand(x.shape[0], self.victim_dims[0], self.attack_dims[1], self.attack_dims[2])
+        X[:, :, start_x:end_x, start_y:end_y] = x
+
+        return X
+    
+    def output_mapper(self, y: torch.Tensor) -> torch.Tensor:
+        return y[:, :10]
---
+++ mnist_attack.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from adv_reprogram.program import MNISTReprogram
+from torchvision import models, datasets, transforms
+from torchmetrics import Accuracy
+
+
+LR_DECAY = 0.05
+LAMBDA = 0.05
+BATCH_SIZE = 100
+NUM_EPOCHS = 10
+
+
+def main(dataset_root: str):
+
+    device = torch.device('cuda')
+
+    model = models.resnet50(weights=models.resnet.ResNet50_Weights.DEFAULT).to(device)
+    model.eval()
+
+    adv_program = MNISTReprogram(
+        attack_dims=(1, 28, 28),
+        victim_dims=(3, 224, 224), 
+        network=model
+    )
+
+    parallel_module = nn.DataParallel(adv_program).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(parallel_module.parameters(), weight_decay=LAMBDA)
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=LR_DECAY)
+
+    train_dataset = datasets.MNIST(root=dataset_root, download=True, train=True, transform=transforms.ToTensor())
+    eval_dataset = datasets.MNIST(root=dataset_root, download=True, train=False, transform=transforms.ToTensor())
+
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    eval_dataloader = DataLoader(eval_dataset, batch_size=BATCH_SIZE, num_workers=2)
+
+    # Training
+    for epoch in range(1, 1 + NUM_EPOCHS):
+
+        # Train over all batches
+        for inputs, labels in tqdm(train_dataloader):
+            outputs = parallel_module(inputs)
+            loss = criterion(outputs.to(device), labels.to(device))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        lr_scheduler.step()
+        
+        # Evaluate over all batches
+        eval_accuracy = Accuracy()
+        for inputs, labels in tqdm(eval_dataloader):
+            outputs = parallel_module(inputs).argmax(dim=1)
+            eval_accuracy(outputs, labels)
+        
+        print(f'epoch [{epoch}/{NUM_EPOCHS}] | Eval Accuracy = {eval_accuracy.compute()}')
+
+
+if __name__ == '__main__':
+    main()
